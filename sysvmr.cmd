decc6b67ccdbaf60dfe36b50cf1fd1f2d108b5ba
Hector Peraza <hperaza65@gmail.com>
2019-06-16 13:12:27 +0200

    TCB: T.TI field now contains a pointer to the UCB of terminal.

---
+++ sysvmr.cmd
@@ -0,0 +1,41 @@
+rsx180
+set /par=syspar:0:16:task
+set /par=ldrpar:16:1:task
+set /par=fcppar:17:3:task
+;set /par=gen:*:*:sys
+set /par=gen:20:236:sys
+par
+ins ldr
+fix ldr
+ins tkn
+fix tktn
+ins mcr
+ins sys
+ins hel
+ins bye
+ins mou
+ins dmo
+ins ufd
+ins ini
+ins rmd
+ins pip
+ins bro
+ins mac
+ins tkb/inc=30000
+ins lbr
+ins mkt
+ins zap
+ins who
+ins dmp
+ins mce
+ins uptime
+ins vdo
+ins view/task=...mor
+ins basic/inc=5000
+ins ccl/task=...ca.
+ins ted/inc=10000/pri=65
+ins md5
+ins dcu
+ins cpu
+ins cal
+tas
